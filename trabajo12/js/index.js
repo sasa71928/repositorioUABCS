2396ab3ce1a413d8b7c6c4bc1c44d6afcd6d9e85
Oscar <osalazarsalas@gmail.com>
2024-10-16 17:40:13 -0700

    calculadora

--- trabajo12/js/index.js
+++ trabajo12/js/index.js
@@ -1,2 +1,24 @@
+function calcular(operacion) {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    let resultado;
 
-console.log('Hola mundo');
+    switch (operacion) {
+        case 'sumar':
+            resultado = num1 + num2;
+            break;
+        case 'restar':
+            resultado = num1 - num2;
+            break;
+        case 'multiplicar':
+            resultado = num1 * num2;
+            break;
+        case 'dividir':
+            resultado = num1 / num2;
+            break;
+        default:
+            resultado = 'Operación no válida';
+    }
+
+    document.getElementById('resultado').innerText = 'Resultado: ' + resultado;
+}
